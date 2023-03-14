c62ec95b209a3e04c41abd48da5a4f47f929bf9e
sayli sunil katte <kattesayli@gmail.com>
2023-03-14 12:37:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/explicitintent2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.explicitintent2
+
+import android.content.Intent
+import android.os.Build.VERSION_CODES.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnIntent=findViewById<Button>(R.id.button)
+
+        btnIntent.setOnClickListener {
+            intent= Intent(applicationContext,MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
